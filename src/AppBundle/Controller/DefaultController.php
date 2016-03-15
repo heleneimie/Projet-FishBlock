c9429d50b1c3e10422351f1522e781bb42cd0439
heleneimie <helene.imie@gmail.com>
2016-03-15 11:36:16 +0100

    Remove .idea/* - Modification base.html.twig + index.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,9 +14,10 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('main/index.html.twig',
+            array('nom' => 'Hélène'
+            )
+        );
     }
 
     /**
