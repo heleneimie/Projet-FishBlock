65254ae3809ebfcfccbba80e34e95847374e0fc4
heleneimie <helene.imie@gmail.com>
2016-03-25 11:09:42 +0100

    Changes on EpisodeController

--- src/AppBundle/Controller/EpisodeController.php
+++ src/AppBundle/Controller/EpisodeController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Serie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -42,6 +43,7 @@ class EpisodeController extends Controller
     public function newAction(Request $request)
     {
         $episode = new Episode();
+//        $episode->setSerie($serie);
         $form = $this->createForm('AppBundle\Form\EpisodeType', $episode);
         $form->handleRequest($request);
 
